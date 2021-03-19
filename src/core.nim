68b09820845ead716e56e07ebf44138e28bd8e09
Kenneth Brown <siegel.zero@gmail.com>
2021-03-19 13:29:32 -0700

    Updated gitignore and added tests

--- src/core.nim
+++ src/core.nim
@@ -10,17 +10,29 @@ func integerSqrt(n: int): int =
     x, y: int
 
   x = 1 shl ((bits shr 1) + 1)
-
   while true:
     y = (x + n div x) shr 1
     if y >= x: return x
     x = y
 
 
+func isSquare(n: int): bool =
+  # Returns `true`
+  let root = integerSqrt(n)
+  return root*root == n
+
+
 when isMainModule:
+  # integerSqrt
   var root: int
-  for i in 0..10000:
+  for i in 0..1000:
     root = integerSqrt(i)
     doAssert root*root <= i
     doAssert i < (root + 1)*(root + 1)
 
+  # isSquare
+  var count = 0
+  for i in 0..1000:
+    if isSquare(i):
+      count += 1
+  doAssert count == 32
